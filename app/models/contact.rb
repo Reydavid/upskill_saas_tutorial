38a12afd7bd6bad833c27dae60412df93eaa96b7
David Sartin <bambam@msn.com>
2016-12-29 21:35:33 +0000

    Added code comments

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,8 @@
 class Contact < ActiveRecord::Base
-  validates :name, :email, :comments, presence: true
+  # Contact form validations
+  # statement on line 4 is combined. Error on contact form submission so I'll revert to long form
+  # validates :name, :email, :comments, presence: true
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :comments, presence: true
 end
